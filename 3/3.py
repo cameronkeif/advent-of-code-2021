ce47a77fb431aaf21221a34dce8b8ecb6a3b8c16
Cameron Keif <cameron.keif@gmail.com>
2021-12-18 18:09:07 -0600

    puzzle 3

---
+++ 3/3.py
@@ -0,0 +1,18 @@
+with open('input.txt') as file:
+    directions = file.readlines()
+
+xOffset = 0
+yOffset = 0
+
+for direction in directions:
+  direction = direction.strip().split(' ')
+  distance = int(direction[1])
+  match direction[0]:
+    case 'forward':
+      xOffset += distance
+    case 'down':
+      yOffset += distance
+    case 'up':
+      yOffset -= distance
+
+print('Total distance: ' + str(xOffset * yOffset))
